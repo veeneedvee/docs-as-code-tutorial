7b103fa3ad05c3681750a48eeff4938b61165593
Varun C Kanthety (l0w3Rc4$T) <vc.kanthety@gmail.com>
2025-04-19 00:28:58 +0530

    GitHub Page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://veeneedvee.github.io/',
+  url: 'https://veeneedvee.github.io/docs-as-code-tutorial/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs-as-code-tutorial/',
